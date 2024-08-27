d7ee14f71dd5bd3216785afa29f6b9b9d631c3ba
sheepo15 <shaoyang.chong@aspen-spectra.com>
2024-08-27 15:53:17 +0800

    Created bottom navigation and fragments

--- app/src/main/java/com/utar/plantogo/MainActivity.java
+++ app/src/main/java/com/utar/plantogo/MainActivity.java
@@ -1,24 +1,97 @@
 package com.utar.plantogo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
+    private OnBackPressedCallback onBackPressedCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        toolbar = findViewById(R.id.toolbar);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(false);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                navigateToFragment(new HomeFragment(), true, false);
+                return true;
+            } else if (itemId == R.id.nav_planner) {
+                navigateToFragment(new PlannerFragment(), true, false);
+                return true;
+            } else if (itemId == R.id.nav_setting) {
+                navigateToFragment(new SettingFragment(), true, false);
+                return true;
+            }
+
+            return false;
         });
+
+        if (savedInstanceState == null) {
+            bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        }
+
+        // Register the OnBackPressedCallback
+        onBackPressedCallback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+                    getSupportFragmentManager().popBackStack();
+                } else {
+                    finish();
+                }
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, onBackPressedCallback);
+    }
+
+    public void navigateToFragment(Fragment fragment, boolean showBottomNav, boolean showActionBar) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        if (showBottomNav) {
+            bottomNavigationView.setVisibility(View.VISIBLE);
+        } else {
+            bottomNavigationView.setVisibility(View.GONE);
+        }
+
+        if (showActionBar) {
+            toolbar.setVisibility(View.VISIBLE);
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationOnClickListener(v -> {
+                if (onBackPressedCallback.isEnabled()) {
+                    onBackPressedCallback.handleOnBackPressed();
+                }
+            });
+        } else {
+            toolbar.setVisibility(View.GONE);
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(false);
+        }
     }
 }
