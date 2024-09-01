c83a44bce77fbeae55b07a112d5530c1f5c47f0e
sheepo15 <shaoyang.chong@aspen-spectra.com>
2024-09-01 23:50:15 +0800

    Created Carousel Adapter

---
+++ app/src/main/java/com/utar/plantogo/ui/carousel/CarouselAdapter.java
@@ -0,0 +1,59 @@
+package com.utar.plantogo.ui.carousel;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.utar.plantogo.internal.tripadvisor.model.Location;
+import com.utar.plantogo.ui.attraction.AttractionCarouselComponent;
+
+import java.util.List;
+
+public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.ViewHolder> {
+
+    private final Context context;
+    private final List<Location> data;
+
+    public CarouselAdapter(Context context, List<Location> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        AttractionCarouselComponent attractionCarouselComponent = new AttractionCarouselComponent(context);
+        return new ViewHolder(attractionCarouselComponent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Location location = data.get(position);
+
+        if (location.getPhotos() != null && !location.getPhotos().isEmpty()) {
+            String imageUrl = location.getPhotos().get(0).getImages().getMedium().getUrl();
+            String attractionName = location.getName();
+            holder.attractionCarouselComponent.setImage(imageUrl);
+            holder.attractionCarouselComponent.setAttractionName(attractionName);
+        } else {
+            holder.attractionCarouselComponent.setImage(null); // Placeholder will be shown
+            holder.attractionCarouselComponent.setAttractionName(null);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        AttractionCarouselComponent attractionCarouselComponent;
+
+        public ViewHolder(@NonNull AttractionCarouselComponent itemView) {
+            super(itemView);
+            this.attractionCarouselComponent = itemView;
+        }
+    }
+}
