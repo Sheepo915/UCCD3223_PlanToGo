7598f4c56b8b57b0554956c09bfb6bba37289a67
sheepo15 <shaoyang.chong@aspen-spectra.com>
2024-09-03 12:54:00 +0800

    Updated search fragment to accommodate fragment view model

--- app/src/main/java/com/utar/plantogo/SearchFragment.java
+++ app/src/main/java/com/utar/plantogo/SearchFragment.java
@@ -2,11 +2,23 @@ package com.utar.plantogo;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.utar.plantogo.internal.tripadvisor.model.Location;
+import com.utar.plantogo.ui.viewmodel.FragmentViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +29,13 @@ public class SearchFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String SEARCH_QUERY = "search_query";
+    private static final String  PRELOAD_DATA = "preload_data";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String searchQuery;
+    private List<Location> preloadData;
+    private FragmentViewModel fragmentViewModel;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -32,16 +45,13 @@ public class SearchFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param searchQuery Search query that will be passed to Tripadvisor API to perform location search.
      * @return A new instance of fragment FragmentPlanner.
      */
-    // TODO: Rename and change types and number of parameters
-    public static PlannerFragment newInstance(String param1, String param2) {
-        PlannerFragment fragment = new PlannerFragment();
+    public static SearchFragment newInstance(String searchQuery) {
+        SearchFragment fragment = new SearchFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(SEARCH_QUERY, searchQuery);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,9 +59,9 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            searchQuery = getArguments().getString(SEARCH_QUERY);
         }
     }
 
@@ -59,6 +69,74 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        // Get the data from view model
+        fragmentViewModel = new ViewModelProvider(requireActivity()).get(FragmentViewModel.class);
+        preloadData = fragmentViewModel.getPreloadData();
+
+        configureToolbarForSearchFragment();
+        configureBottomNavigationBarVisibility(false);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        configureBottomNavigationBarVisibility(true);
+        configureToolbarOnDestroy();
+    }
+
+    /**
+     * Custom configuration for search fragment toolbar
+     */
+    private void configureToolbarForSearchFragment() {
+        AppCompatActivity activity = (AppCompatActivity) requireActivity();
+
+        Toolbar toolbar = activity.findViewById(R.id.toolbar);
+        activity.setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(activity.getSupportActionBar()).setTitle("Search");
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(v -> {
+            requireActivity().onBackPressed(); // Handle back press
+        });
+
+        View profileHeaderContainer = activity.findViewById(R.id.cl_profile_header_container);
+        if (profileHeaderContainer != null) {
+            profileHeaderContainer.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * Configure the visibility of the bottom navigation bar during change in fragment
+     * @param visible true if visible, false if gone
+     */
+    private void configureBottomNavigationBarVisibility(boolean visible) {
+        BottomNavigationView bottomNavigationView = requireActivity().findViewById(R.id.bottom_navigation_bar);
+
+        if (bottomNavigationView != null) {
+            bottomNavigationView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    /**
+     * Configure the toolbar during fragment destroy in fragment lifecycle
+     */
+    private void configureToolbarOnDestroy() {
+        AppCompatActivity activity = (AppCompatActivity) requireActivity();
+
+        Toolbar toolbar = activity.findViewById(R.id.toolbar);
+        activity.setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(activity.getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        View profileHeaderContainer = activity.findViewById(R.id.cl_profile_header_container);
+        if (profileHeaderContainer != null) {
+            profileHeaderContainer.setVisibility(View.VISIBLE);
+        }
     }
 }
