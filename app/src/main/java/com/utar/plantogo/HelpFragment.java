3a199e496aa4fae07aa0d4a76d043ca11ea1cc2b
sheepo15 <shaoyang.chong@aspen-spectra.com>
2024-09-05 21:54:55 +0800

    Added help fragment and terms and condition fragment

---
+++ app/src/main/java/com/utar/plantogo/HelpFragment.java
@@ -0,0 +1,27 @@
+package com.utar.plantogo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HelpFragment extends Fragment {
+    public HelpFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+
+        return view;
+    }
+}
