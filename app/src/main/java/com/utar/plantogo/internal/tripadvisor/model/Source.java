e5ee9ad7695035b94008c9909df49ee9095a7a19
sheepo15 <shaoyang.chong@aspen-spectra.com>
2024-09-07 16:24:32 +0800

    Added getter and setter

--- app/src/main/java/com/utar/plantogo/internal/tripadvisor/model/Source.java
+++ app/src/main/java/com/utar/plantogo/internal/tripadvisor/model/Source.java
@@ -4,6 +4,19 @@ public class Source {
     private String name;
     private String localizedName;
 
-    // Getters and setters
-    // ...
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocalizedName() {
+        return localizedName;
+    }
+
+    public void setLocalizedName(String localizedName) {
+        this.localizedName = localizedName;
+    }
 }
